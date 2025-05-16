948bc79f893ca5bc4660326480756bd47e656dd9
Selina789 <selinalim7890@gmail.com>
2025-05-16 20:50:56 +0700

    change desc and button in landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,10 +13,10 @@ const Landing = () => {
         <h1 className="landing-headline text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold leading-tight mb-4">
           TRAVEL AROUND <br/> THE WORLD
         </h1>
-        <p className="landing-headline text-lg sm:text-xl md:text-2xl lg:text-3xl mb-6 font-medium tracking-wide drop-shadow-lg drop-shadow-red-500/50 landing-subline">
+        <p className="landing-headline text-lg sm:text-lg md:text-xl lg:text-2xl mb-6 font-medium tracking-wide drop-shadow-lg drop-shadow-red-500/50 landing-subline">
           TRIP WITHOUT HASSLE, ONLY WITH MSTRAVELINDO
         </p>
-        <button className="slowmo-button-hover font-bold bg-red-400 text-white py-3 px-8 rounded-lg text-xl sm:text-2xl md:text-3xl hover:bg-red-600 transition tracking-wider animation">
+        <button className="slowmo-button-hover font-bold bg-red-400 text-white py-3 px-8 rounded-lg text-lg sm:text-xl md:text-2xl hover:bg-red-600 transition tracking-wider animation">
           PLAN YOUR TRIP
         </button>
       </div>
