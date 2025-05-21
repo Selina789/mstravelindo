82db82ea83de2ae511cc4196113f0684a959d969
Selina789 <selinalim7890@gmail.com>
2025-05-21 23:37:08 +0700

    spacing change

--- src/pages/tickets/PlaneTickets.jsx
+++ src/pages/tickets/PlaneTickets.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import backArrow from '../../assets/back-arrow.png';
 import Footer from "../../components/Footer";
+import '../../index.css';
 
 const tours = [
   {
